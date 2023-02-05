e7ff53b4ebb1f589471b6635dd7a7ae70569cc60
Salazar <salazar97@protonmail.com>
2023-02-05 16:45:58 -0600

    nuevos

---
+++ Hola mundo.py
@@ -0,0 +1,4 @@
+print("Hola mundo")
+print("Jesus Manuel Alaniz Salazar")
+print("Aprendiedno phyton")
+print("Esto es una prctica")
---
+++ variables.py
@@ -0,0 +1,11 @@
+nombre ="Jesus"
+print(nombre)
+
+sueldo = 1025.45
+print(sueldo)
+
+contenido = "Metodo de aprendizaje"
+print(contenido)
+
+contenido = 250.34
+print(contenido)
