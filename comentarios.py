c4c54daff70072ccffa8858566666e93c08fb53e
Salazar <salazar97@protonmail.com>
2023-02-05 20:17:30 -0600

    Nuevo

---
+++ comentarios.py
@@ -0,0 +1,7 @@
+#comentario 1
+#comentario 2
+
+""" Curso de aprendizaje de python desde Cero
+Nivel Basico
+Nuvel Intermedio
+Nivel Avanzado """
---
+++ operaciones_mat.py
@@ -0,0 +1,32 @@
+"""
+Operaciones matemáticas con python
+python nos permite realizar las siguientes operaciones matemáticas.
+A continuación presentamos cada una con su operdador respectivo
+
+Suma +
+Resta -
+Multiplicación *
+División /
+División entera //
+Módulo (resta) %
+Potenciación **
+"""
+num1=150
+num2=200
+num3=1035
+num4=100
+
+resultado_suma= num1+num2
+resultado_resta= num2-num1
+resultado_multiplica= num1*num2
+resultado_division= num3/num4
+resultado_division_exacta= num3//num4
+resultado_division_residuo= num3%num4
+
+print("resultado_suma", resultado_suma)
+print("resultado_resta", resultado_resta)
+print("resultado_multiplica", resultado_multiplica)
+print("resultado_division", resultado_division)
+print("resultado_division_exacta", resultado_division_exacta)
+print("resultado_division_residuo", resultado_division_residuo)
+print("potenciación", 5**3)
