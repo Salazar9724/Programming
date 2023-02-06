04e62e0b55802fe43b00973913f5265c4e7e2399
Salazar <salazar97@protonmail.com>
2023-02-05 18:57:46 -0600

    Nuevo

---
+++ Tipos de datos.py
@@ -0,0 +1,6 @@
+Num1 = 120
+Num2 = 135.45
+Texto = "Curso de aprendisaje"
+Nombre = "Jesus"
+Estado = True
+print(type(Estado))
---
+++ conversiones de datos.py
@@ -0,0 +1,15 @@
+num1="120"
+num2="150"
+num3=103
+sueldo=1035.78
+texto="curso de aprendizaje"
+#int(x) convierte x a entero.
+#float(x) convierte x en un número de punto flotante.
+#str(x) convierte x en una cadena.
+#hex(x) convierte x enteri en una cadena hexadecimal.
+
+print(int(num1)+int(num2))
+print(int(sueldo))
+print("sueldo mensual:"  +str(sueldo))
+print(float(num3))
+print(hex(num3))
